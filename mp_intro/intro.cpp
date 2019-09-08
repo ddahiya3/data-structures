cdfa7ddb8e35bf63b18da0e2a07f31d7d860d93e
ddahiya3 <ddahiya3@illinois.edu>
2019-09-08 14:46:23 -0500

    Makefile and rotate image done for mp_intro

--- mp_intro/intro.cpp
+++ mp_intro/intro.cpp
@@ -3,10 +3,39 @@
 
 #include <string>
 
+using namespace cs225;
+
 
 void rotate(std::string inputFile, std::string outputFile) {
-  // TODO: Part 2
+
+  PNG* input = new PNG();
+  input->readFromFile(inputFile);
+
+  unsigned width = input->width();
+  unsigned height = input->height();
+
+  PNG* output = new PNG(width, height);
+
+  for (unsigned y = 0; y < height; y ++) {
+    for (unsigned x = 0; x < width; x ++) {
+
+      HSLAPixel& afterRotation = input->getPixel(width - x - 1, height - y - 1);
+
+      HSLAPixel& currOutPixel = output->getPixel(x,y);
+
+      currOutPixel = afterRotation;
+
+    }
+  }
+
+  output->writeToFile(outputFile);
+
+  delete input;
+  delete output;
+
 }
+
+
 
 cs225::PNG myArt(unsigned int width, unsigned int height) {
   cs225::PNG png(width, height);
