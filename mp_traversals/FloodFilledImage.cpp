dd0b74d40ccc75f77606e555b13b74aea93c1fd1
ddahiya3 <ddahiya3@illinois.edu>
2019-10-21 19:01:27 -0500

    mp_traversals done

--- mp_traversals/FloodFilledImage.cpp
+++ mp_traversals/FloodFilledImage.cpp
@@ -57,84 +57,19 @@ void FloodFilledImage::addFloodFill(ImageTraversal & traversal, ColorPicker & co
  */ 
 Animation FloodFilledImage::animate(unsigned frameInterval) const {
   Animation animation;
-  /** @todo [Part 2] */
-  /*
-  animation.addFrame(*image_);
-
-  for (unsigned i = 0; i < traversal_queue_.size(); i++) {
-
-    ImageTraversal::Iterator it_begin = traversal_queue_[i]->begin();
-    ImageTraversal::Iterator it_end = traversal_queue_[i]->end();
-
-    int counter = 0;
-
-    for (ImageTraversal::Iterator k = it_begin; k != it_end; ++k) {
-
-      if (counter == (int)frameInterval) {
-        animation.addFrame(*image_);
-        counter = 0;
-      }
-
-      counter++;
-
-      HSLAPixel & image_pixel = image_->getPixel((*k).x, (*k).y);
-      HSLAPixel color_pixel = color_queue_[i]->getColor((*k).x, (*k).y);
-
-      image_pixel.h = color_pixel.h;
-      image_pixel.l = color_pixel.l;
-      image_pixel.a = color_pixel.a;
-      image_pixel.s = color_pixel.s;
-    }
-
-    animation.addFrame(*image_);
-
-  }
-
-  return animation;
-  */
-	animation.addFrame(*image_);
   int counter = 0;
-
-  for (unsigned i = 0; i < traversal_.size(); ++i) {
-
-    for (ImageTraversal::Iterator it = traversal_[i]->begin(); it != traversal_[i]->end(); ++it) {
-      
-      if (counter % (int)frameInterval == 0) {
+  
+  //animation.addFrame(*image_);
+  for (unsigned i = 0; i < traversal_.size(); i++) {
+    for (const Point & p : *traversal_[i]) {
+      if ((counter % (int)frameInterval) == 0) {
         animation.addFrame(*image_);
       }
-
-      HSLAPixel & old_pixel = image_->getPixel((*it).x, (*it).y);
-      old_pixel = color_[i]->getColor((*it).x, (*it).y);
-
-      
-
+      HSLAPixel & pixel = image_->getPixel(p.x,p.y);
+      pixel = color_[i]->getColor(p.x,p.y);
       counter++;
     }
     animation.addFrame(*image_);
   }
-  
-/*
-	std::list<ImageTraversal*>::iterator it_traverse;
-	std::list<ColorPicker*>::iterator it_color = color_.begin();
-	for (it_traverse = traversal_.begin(); it_traverse != traversal_.end(); ++it_traverse)
-	{
-		ImageTraversal* telement = *(it_traverse);
-		ColorPicker* celement = *(it_color);
-		for (ImageTraversal::Iterator it = telement->begin(); it!=telement->end(); ++it)
-		{
-			unsigned x_coord = (*it).x;
-			unsigned y_coord = (*it).y;
-			HSLAPixel & p1 = image_.getPixel(x_coord, y_coord);
-			p1 = celement->getColor(x_coord, x_coord);
-			frame++;
-			//cout << frame << endl;
-			if (frame%frameInterval==0)
-				animation.addFrame(image_);
-		}
-		++it_color;
-	}
-	animation.addFrame(image_);
-  */
-  
 	return animation;
 }
